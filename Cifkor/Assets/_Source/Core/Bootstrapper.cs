5c2ab322a8f8f2b20fe813dbec3066b912ca117a
Anton4ik-dev <afedot2004@gmail.com>
2025-03-18 14:50:08 +0300

    feat: initial commit

---
+++ Cifkor/Assets/_Source/Core/Bootstrapper.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace Core
+{
+    public class Bootstrapper : MonoBehaviour
+    {
+        private void Awake()
+        {
+
+        }
+    }
+}
---
+++ Cifkor/Assets/_Source/Core/MainInstaller.cs
@@ -0,0 +1,12 @@
+using Zenject;
+
+namespace Core
+{
+    public class MainInstaller : MonoInstaller
+    {
+        public override void InstallBindings()
+        {
+
+        }
+    }
+}
